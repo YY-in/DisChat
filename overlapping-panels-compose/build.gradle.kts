cc161fa77d3fecf6b473c9243b882de24aef72c4
YY-in <139803551@qq.com>
2022-10-03 22:34:25 +0800

    feat: change gradle to use kts with buildSrc

---
+++ overlapping-panels-compose/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        minSdk = 21
+
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = false
+        resValues = false
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = Dependencies.Compose.compilerVersion
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+        freeCompilerArgs = freeCompilerArgs +
+                "-Xexplicit-api=strict" +
+                "-Xopt-in=androidx.compose.material.ExperimentalMaterialApi"
+    }
+}
+
+dependencies {
+    implementation(Dependencies.Compose.foundation)
+    implementation(Dependencies.Compose.material)
+}
